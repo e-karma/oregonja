2a8e221840440230cf1fbbbf17e7173adb1f008d
e-karma <djbradyla@gmail.com>
2015-07-08 05:41:38 -0400

    todos posting and loading

--- client/app/pods/s/todos/create/route.js
+++ client/app/pods/s/todos/create/route.js
@@ -9,7 +9,7 @@ export default Ember.Route.extend(DestroyNew, {
         createTodo(model) {
             // model.set('tags', ['lifestyle', 'health', 'tech']);
             var userId = this.session.get('user.id');
-            this.store.find('user', userId).then(result => {
+            this.store.findRecord('user', userId).then(result => {
                 model.set('user', result);
                 return model.save();
             }).then(todo => this.transitionTo('s.todos.todo', todo));

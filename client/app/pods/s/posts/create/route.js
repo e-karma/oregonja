6bb17bece818f95eacddacd2d065426c5a971f30
Igor Preston <igorpreston@gmail.com>
2015-07-12 21:09:02 +0300

    fixed the issue with getting user id from session

--- client/app/pods/s/posts/create/route.js
+++ client/app/pods/s/posts/create/route.js
@@ -8,7 +8,7 @@ export default Ember.Route.extend(DestroyNew, {
     actions: {
         createPost(model) {
             // model.set('tags', ['lifestyle', 'health', 'tech']);
-            var userId = this.session.get('user.id');
+            var userId = this.get('session.secure.user.id');
             this.store.findRecord('user', userId).then(result => {
                 model.set('user', result);
                 return model.save();

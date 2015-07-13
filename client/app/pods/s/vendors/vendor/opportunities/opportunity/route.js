9aa660ea5a7a32e6b90cd031c62ea481fa166855
Igor Preston <igorpreston@gmail.com>
2015-07-14 01:54:17 +0300

    implemented opportunity view

--- client/app/pods/s/vendors/vendor/opportunities/opportunity/route.js
+++ client/app/pods/s/vendors/vendor/opportunities/opportunity/route.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model: function(params) {
+    this.store.find('opportunity', params.employee_id)
+  },
+
+  actions: {
+
+    willTransition: function() {
+      this.controllerFor('s.vendors.vendor.opportunities.opportunity').set('isShowingOpportunitySuccess', false);
+    }
+
+  }
 });

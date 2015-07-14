74d9cae287e5bf1efea5c9d4582b1f1b3648ae6e
Igor Preston <igorpreston@gmail.com>
2015-07-14 04:25:39 +0300

    implemented changing the stage of opportunity

--- client/app/pods/s/vendors/vendor/opportunities/opportunity/controller.js
+++ client/app/pods/s/vendors/vendor/opportunities/opportunity/controller.js
@@ -1,5 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isShowingOpportunitySuccess: false
+  isShowingOpportunitySuccess: false,
+  isChangingStage: false,
+  stages: ["Demo", "Evaluation", "Solutions Planning Call", "Quote", "Quote Review", "Closed", "Won & Closed", "Lost"],
+
+  actions: {
+    changeStage: function() {
+      this.toggleProperty('isChangingStage');
+    },
+
+    selectStage: function(stage) {
+      this.set('selectedStage', stage);
+      this.set('model.stage', stage);
+    },
+
+    saveOpportunity: function() {
+      this.get('model').save();
+      return false;
+    }
+  }
 });
